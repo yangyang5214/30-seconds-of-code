57952cad25e42a1bf7e2bd9a9013c01f5195d87f
yangyang5214 <beer5214@126.com>
2021-05-28 08:38:52 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,43 @@
 
 set -ex
 
-cd 30-seconds-of-code
+rm -rf source/_posts/*
 
-# git pull
+function addTag(){
+	cd $1
+	git pull
 
-cp -r snippets/* ../source/_posts/
+	_dir=../source/_posts/$1
 
-cd ..
+	mkdir -p $_dir
 
-cd source/_posts
+	cp -r snippets/* $_dir
+	
+	cd $_dir
 
+	for file in `ls`
+	do
+		target_file=$2'_'$file
+		mv $file $target_file
+        	sed -i '3s/: /: [/g; 3s/$/',$2']/g' $target_file
+        	image_url=$(curl https://www.hexianwei.com/api/bed/random)
+        	sed -i '5a![]('$image_url')' $target_file
+	done
 
-for file in `ls`
-do 
- 	sed -i '3s/: /: [/g; 3s/$/]/g' $file
-	image_url=$(curl https://www.hexianwei.com/api/bed/random)
-	sed -i '5a![]('$image_url')' $file
-done
+	mv * ../
 
-cd ../../
+	cd ..
+
+	rm -rf $1
+
+	cd ../../
+}
+
+addTag '30-seconds-of-code' 'js'
+addTag '30-seconds-of-git' 'git'
+addTag '30-seconds-of-golang' 'golang'
+addTag '30-seconds-of-python' 'python'
+addTag '30-seconds-of-css' 'css'
+addTag '30-seconds-of-react' 'react'
 
 hexo g
